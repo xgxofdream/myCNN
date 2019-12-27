af1293817073c689e3b49260121c75835df9af6c
xgxofdream <liujie.dhu@gmail.com>
2019-12-28 02:23:56 +1000

    Update myCNN.py

--- myCNN.py
+++ myCNN.py
@@ -10,9 +10,10 @@ Modified on Wed Dec 24 10:24:47 2019
 """
 import cv2
 import os
-import keras
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow.keras import layers, models
 
 #--------------------------------------基本参数配置-----------------------------
@@ -28,7 +29,7 @@ save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
 
 #迭代次数
-epoch_num = 1
+epoch_num = 20
 
 #初始化
 images = []
@@ -80,6 +81,7 @@ x_test, y_test = load_dataset(test_dir)
 x_train, y_train = load_dataset(train_dir)
 
 #归一化数据（可以不要）
+#注意，数据作为整数加载，因此我们必须将其转换为浮点值才能执行除法。
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -90,8 +92,11 @@ x_test /= 255
 # Convert class vectors to binary class matrices.
 # 但是进入训练进程后，会报错：
 # Can not squeeze dim[1], expected a dimension of 1, got 10 for 'metrics/accuracy/Squeeze' (op: 'Squeeze') with input shapes: [?,10].
+# 原因是 Tensorflow不支持这种标签定义方式：The TensorFlow documentation clearly states that "labels vector must provide a single specific index for the true class for each row of logits".
+# 所以还是直接采用10进制整数吧！
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+
 '''
 #验证数据集信息
 print("Train data: ", x_train.shape)
@@ -100,18 +105,14 @@ print("Test data: ", x_test.shape)
 print("Test labels: ", y_test.shape)
 
 
-plt.figure(figsize=(10,10))
-for i in range(9):
-        plt.subplot(3,3,i+1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(x_train[i], cmap=plt.cm.binary)
-
-plt.show()
+fig = plt.figure(figsize=(10,10))
+for i in range(25):
+    ax = fig.add_subplot(5, 5, i + 1, xticks=[], yticks=[])
+    ax.imshow(np.squeeze(x_train[i]))
 
 
 #--------------------------------------Keras网络配置-----------------------------
+# Create the model
 # the following code is from 
 # https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/images/cnn.ipynb#scrollTo=0LvwaKhtUdOo
 # it is based on Keras directly, not using tensorflow as backend    
@@ -121,24 +122,36 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.summary()
     
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 model.summary()
-    
+
+# Compile model
+# 梯度下降是目前神经网络中使用最为广泛的优化算法之一。
+# 为了弥补朴素梯度下降的种种缺陷，研究者们发明了一系列变种算法，从最初的 SGD (随机梯度下降) 逐步演进到 NAdam。
+# 然而，许多学术界最为前沿的文章中，都并没有一味使用 Adam/NAdam 等公认“好用”的自适应算法，很多甚至还选择了最为初级的 SGD 或者 SGD with Momentum 等。   
+# 这里用的Adam优化器，SGD优化器可以调整参数
+# sgd = SGD(lr=0.01, momentum=0.9, decay=decay, nesterov=False)
 model.compile(optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy'])
 
+#--------------------------------------Tensorboard配置-----------------------------
+# 可能由于没用Tensorflow的原因，logs没有被创建
+# 用一个"\"取消第二个"\"的特殊转义作用，即为"\\"
+# Tensorboard usage: python -m tensorboard.main --logdir=logs/fit
+log_dir='logs\\fit\\' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 #--------------------------------------训练模型-----------------------------
-# 迭代
+# Fit the model
 history = model.fit(x_train, y_train, epochs=epoch_num, 
                     validation_data=(x_test, y_test))
 
 #--------------------------------------保存模型-----------------------------
-# Save model and weights
+# Save model
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 model_path = os.path.join(save_dir, model_name)
@@ -156,4 +169,4 @@ plt.legend(loc='lower right')
 
     
 test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
-print(test_acc)
+print("Accuracy: %.2f%%" % (test_acc*100))
